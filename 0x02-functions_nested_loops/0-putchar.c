5ef390c7466d043742aba1de1d83542c2ad3aac6
Samuel Chigozie <nzekwesammy@gmail.com>
2023-02-21 03:22:01 -0800

    My 0 task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,19 @@
-#include <unistd.h>
-int _putchar(char c)
+#include "main.h"
+
+/**
+ * main - Print _putchar
+ * Return: Always 0
+ */
+int main(void)
 {
-return(write(0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
