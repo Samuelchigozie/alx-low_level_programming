6a9cc7ac4562f0f62de73ef21d4a8ca9ca3fcfe0
Samuel Chigozie <nzekwesammy@gmail.com>
2023-04-10 18:49:42 -0700

    Task 0

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,6 +1,6 @@
 /**
  * clear_bit - Sets the value of a bit at a given index to 0.
- * @n: A pointer to the bit.
+ * @num: A pointer to the bit.
  * @index: The index where the value needs to be set to 0.
  *
  * Return: If an error occurred, it returns -1. Otherwise, it returns 1.
