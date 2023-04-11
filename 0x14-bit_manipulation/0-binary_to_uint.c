ec81a9f5164815b93c0c7bffa36878639a3fe72c
Samuel Chigozie <nzekwesammy@gmail.com>
2023-04-10 18:43:39 -0700

    Task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,24 +8,24 @@
  */
 unsigned int binary_to_uint(const char *binary_str)
 {
-    unsigned int decimal = 0;
+	unsigned int decimal = 0;
 
-    /* Check for null pointer input */
-    if (!binary_str)
-        return (0);
+	/* Check for null pointer input */
+	if (!binary_str)
+		return (0);
 
-    /* Traverse binary string, converting binary digits to decimal */
-    while (binary_str && *binary_str)
-    {
-        /* Check for invalid binary digit */
-        if (*binary_str < '0' || *binary_str > '1')
-            return (0);
+	/* Traverse binary string, converting binary digits to decimal */
+	while (binary_str && *binary_str)
+	{
+		/* Check for invalid binary digit */
+		if (*binary_str < '0' || *binary_str > '1')
+			return (0);
 
-        /* Convert binary digit to decimal and add to total */
-        decimal = decimal * 2 + *binary_str - '0';
-        binary_str++;
-    }
+		/* Convert binary digit to decimal and add to total */
+		decimal = decimal * 2 + (*binary_str - '0');
+		binary_str++;
+	}
 
-    /* Return the resulting decimal value */
-    return (decimal);
+	/* Return the resulting decimal value */
+	return (decimal);
 }
